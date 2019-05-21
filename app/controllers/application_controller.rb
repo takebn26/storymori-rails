c861636d214c663f83ea46354386d7ea8901c76e
takebn26 <takehaya1026@gmail.com>
2019-05-21 11:52:07 +0900

    rescue 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::RoutingError, with: :res_404
+
+  def res_404
+    render json: { message: "Sorry can't find that!" }, status: :not_found
+  end
 end
