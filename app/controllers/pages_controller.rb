af8e486f5e6138da556a23bffa607e4b49cfb0b1
takebn26 <takehaya1026@gmail.com>
2019-05-22 10:57:38 +0900

    add page create

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,21 @@ class PagesController < ApplicationController
 
     raise ActionController::RoutingError.new('routing error')
   end
+
+  def create
+    page = Page.find_by(id: params[:parent_id], story_id: params[:story_id])
+    raise ResourceError unless page
+
+    child_page = page.children.new(page_params)
+
+    return render json: { id: child_page.id }, status: :created  if child_page.save
+
+    raise ResourceError.new('resource error')
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:name, :text).merge(story_id: params[:story_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :stories, only: %I[ index create ] do
       resources :pages, only: %I[ show ]
+      post '/pages/:parent_id/next' => 'pages#create'
     end
   end
 
