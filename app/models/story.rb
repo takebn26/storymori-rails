6ee1cd82aebac8babb33acef3267bd1c24c234e4
takebn26 <takehaya1026@gmail.com>
2019-05-22 12:26:39 +0900

    fix validation

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,6 @@
 class Story < ApplicationRecord
   has_many :pages
 
-  validates :title, presence: true
-  validates :title, length: { maximum: 10 }
+  validates :title, presence: true, length: { maximum: 10 }
   validates :summary, presence: true
 end
