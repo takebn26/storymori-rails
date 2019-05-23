fd224e2f1ce2bb5b62942155b180853507b4bdd4
takebn26 <takehaya1026@gmail.com>
2019-05-23 10:40:31 +0900

    fix bug

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ApplicationRecord
   has_many :pages, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 10 }
-  validates :summary, presence: true
+  validates :title, presence: true
+  validates :summary, presence: true, length: { maximum: 10 }
 end
